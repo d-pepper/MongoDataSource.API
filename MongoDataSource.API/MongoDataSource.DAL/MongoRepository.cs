14c7ee888bad92238290de78ff2b421886e2c908
Dale Pepper <dale_pepper@hotmail.co.uk>
2016-01-26 22:43:12 +0000

    Simplified DAL to focus on mvp

--- MongoDataSource.API/MongoDataSource.DAL/MongoRepository.cs
+++ MongoDataSource.API/MongoDataSource.DAL/MongoRepository.cs
@@ -1,16 +1,23 @@
 ﻿using MongoDB.Driver;
+using MongoDB.Bson;
 
 namespace MongoDataSource.DAL
 {
-    public class MongoRepository : IRepository
+    public class MongoRepository
     {
         protected static IMongoClient _client;
         protected static IMongoDatabase _database;
 
         public MongoRepository()
         {
-            _client = new MongoClient();
+            var connectionstring = "mongodb://localhost:27017";
+
+            _client = new MongoClient(connectionstring);
             _database = _client.GetDatabase("restaurants");
+            var col = _database.GetCollection<BsonDocument>("restaurants");
+
         }
     }
+
+
 }
